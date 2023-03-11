bd6b1a31bc5853cf7e16c1d37f919174779b9202
sun2002lsw <44559110+sun2002lsw@users.noreply.github.com>
2023-03-11 22:31:55 +0900

    Deploy army

--- Assets/Game/ArmyDeploy.cs
+++ Assets/Game/ArmyDeploy.cs
@@ -9,28 +9,72 @@ public class ArmyDeploy : MonoBehaviour
     [SerializeField]
     private UnitData swordsmanData;
 
-    void Start()
+    void Awake()
     {
-        Vector3 v1 = new Vector3(-1, 1, -1);
-        Unit s1 = spawnSwordsman(v1);
-        s1.tag = "ally";
-
-        Vector3 v2 = new Vector3(1, 1, 1);
-        Unit s2 = spawnSwordsman(v2);
-        s2.tag = "enemy";
+        for (float x = 0; x < 5; x++)
+            for (float y = 10; y < 15; y++)
+                spawnSymmetryArmy(swordsmanPrefab, swordsmanData, new Vector2(x, y));
     }
 
-    Unit spawnSwordsman(Vector3 pos)
+    void spawnSymmetryArmy(GameObject obj, UnitData data, Vector2 pos)
     {
-        return spawnUnit(swordsmanPrefab, swordsmanData, pos);
+        if (pos.x < 0 || pos.y < 10)
+            return;
+
+        // all symmetry positions
+        List<Vector3> positions = new List<Vector3>
+        {
+            new Vector3(pos.x, 1, pos.y),
+            new Vector3(pos.x, 1, -pos.y)
+        };
+
+        if (pos.x != 0)
+        {
+            positions.Add(new Vector3(-pos.x, 1, pos.y));
+            positions.Add(new Vector3(-pos.x, 1, -pos.y));
+        }
+
+        // spawn units
+        foreach (var position in positions)
+            spawnUnit(obj, data, position);
     }
 
-    Unit spawnUnit(GameObject obj, UnitData data, Vector3 pos)
+    GameObject spawnUnit(GameObject obj, UnitData data, Vector3 pos)
     {
-        Unit unit = Instantiate(obj).GetComponent<Unit>();
+        GameObject instance = Instantiate(obj);
+
+        instance.transform.position = pos;
+        lookCenterLine(ref instance);
+        setTagByPosition(ref instance);
+        //setColorByTag(ref instance);
+
+        Unit unit = instance.GetComponent<Unit>();
         unit.UnitData = data;
-        
-        unit.transform.position = pos;
-        return unit;
+
+        return instance;
+    }
+
+    void lookCenterLine(ref GameObject obj)
+    {
+        Vector3 pos = obj.transform.position;
+        if (pos.z > 0)
+            obj.transform.rotation = Quaternion.LookRotation(new Vector3(0, pos.y, -pos.z));
+    }
+
+    void setTagByPosition(ref GameObject obj)
+    {
+        if (obj.transform.position.z > 0)
+            obj.tag = "enemy";
+        else
+            obj.tag = "ally";
+    }
+
+    void setColorByTag(ref GameObject obj)
+    {
+        if (obj.tag == "enemy")
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+            renderer.material.color = Color.red;
+        }
     }
 }
